7aad86de26b29f8ff291ab8d91daa8b06f53022d
Berat Yasin Temel <berat.temel@bilgiedu.net>
2022-03-29 11:30:51 +0300

    first commit

---
+++ PistolFire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolFire : MonoBehaviour
+{
+    public GameObject M9;
+    public bool isFiring = false;
+    public GameObject muzzleFlash;
+    public AudioSource pistolShot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isFiring == false)
+            {
+                StartCoroutine(FireThePistol());
+            }
+        }
+    }
+    IEnumerator FireThePistol()
+    {
+        isFiring = true;
+        M9.GetComponent<Animator>().Play("Fire_Pistol");
+        pistolShot.Play();
+        muzzleFlash.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        muzzleFlash.SetActive(false);
+        yield return new WaitForSeconds(0.25f);
+        M9.GetComponent<Animator>().Play("PistolFire");
+        isFiring = false;
+    }
+}
